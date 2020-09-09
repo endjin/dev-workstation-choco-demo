5571e490d19cf7163fa4ba5ecdbec8a0ce5c8721
James Dawson <james.dawson@endjin.com>
2020-09-09 14:38:41 +0100

    Set execution policy

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,5 @@
 # Install BoxStarter & Chocolatey
+Set-ExecutionPolicy RemoteSigned -Force
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
 
 $starterPackage = 'https://raw.githubusercontent.com/endjin/dev-workstation-choco-demo/master/simple-boxstarter-script.txt'
