affbb73360f5baaa79003d1fe9d5b0b21f227889
James Dawson <james.dawson@endjin.com>
2020-09-10 11:50:05 +0100

    Fix package download path

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -5,7 +5,7 @@ Set-ExecutionPolicy RemoteSigned -Force
 # download test packages into local Boxstarter package feed directory
 @('common-workstation-software','developer-workstation-software') | ForEach-Object {
     $uri = 'https://github.com/endjin/dev-workstation-choco-demo/blob/master/packages/{0}/{0}.0.1.0.nupkg?raw=true' -f $_
-    $file = '{0}\Boxstarter\BuildPackages\{1}-0.1.0.nupkg' -f $env:TEMP, $_
+    $file = '{0}\Boxstarter\BuildPackages\{1}-0.1.0.nupkg' -f $env:ProgramData, $_
     Remove-Item $file -ErrorAction SilentlyContinue
     Write-Host "Downloading pacakge: $uri"
     Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile $file -Headers @{"Cache-Control"="no-cache"} | Out-Null
